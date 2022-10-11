f399bed8043b1a250f9494af77006134f2c78f10
sohampatil <sohampatil.2303@gmail.com>
2022-10-10 22:59:12 -0700

    Created Skeleton for Facade Pattern

---
+++ src/ProductMenu.java
@@ -0,0 +1,8 @@
+public interface ProductMenu {
+    void showMenu();
+    void showAddButton();
+    void showViewButton();
+    void showRadioButton();
+    void showLabels();
+    void showComBoxes();
+}
